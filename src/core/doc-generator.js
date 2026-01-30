7864ac997877310148c6df77fcad8a0b394edc92
Mandefro Endalk <mandefroendalk00@gmail.com>
2026-01-30 22:48:52 +0300

    adding files we want to scan

--- src/core/doc-generator.js
+++ src/core/doc-generator.js
@@ -3,6 +3,24 @@ import fs from "fs-extra";
 import path from "path";
 import chalk from "chalk";
 
-export async function generateDocs() {}
+export async function generateDocs() {
+  console.log(chalk.blue("Analyzing source code for API routes."));
+
+  const root = process.cwd();
+  const sourthPath = path.join(root, "src");
+  const project = new Project({
+    tsConfigFilePath: path.join(root, "tsconfig.json"),
+    skipAddingFilesFromTsConfig: true,
+  });
+
+  project.addSourceFilesAtPaths("src/routes/**/*.ts");
+
+  const routes = [];
+  const sourceFiles = project.getSourceFile();
+
+  for (const sourceFile of sourceFile) {
+    if (sourceFile.getBaseName() === "index.ts") continue;
+  }
+}
 
 export async function buildOpenAPISpec(foundRoutes) {}
